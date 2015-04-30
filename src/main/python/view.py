da889751abab42692c56da51005dd3adf2681c2b
Rich Burdon <rich.burdon@gmail.com>
2015-04-30 09:14:07 -0400

    ok

--- src/main/python/view.py
+++ src/main/python/view.py
@@ -10,11 +10,17 @@ from py2neo import Graph, Node, Relationship
 class HomeView(flask.views.MethodView):
     PATH = '/'
 
-    # TODO(burdon): Build table of instances and test connection to neo4j instance.
     def get(self):
         # https://cloud.google.com/appengine/docs/python/modules/functions
         from google.appengine.api import modules
-        return flask.render_template('hello.html', modules=modules.get_modules())
+        module_map = {}
+        for module in modules.get_modules():
+            version = modules.get_default_version(module)
+            module_map[module] = {
+                'version': version,
+                'hostname': modules.get_hostname(module, version)
+            }
+        return flask.render_template('hello.html', modules=module_map)
 
 
 class DataView(flask.views.MethodView):
@@ -22,7 +28,7 @@ class DataView(flask.views.MethodView):
 
     # TODO(burdon): Init + query paths
     def get(self):
-        graph = Graph('http://192.168.99.100:32776/db/data/')
+        graph = Graph('http://0.0.0.0:7474/db/data/')
         import logging
         logging.info(graph)
 
