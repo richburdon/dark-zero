2cb2d89ed45f4b4072ab29add71f472362b26e40
Rich Burdon <rich.burdon@gmail.com>
2015-05-08 18:18:17 -0400

    ok

---
+++ src/main/python/data.py
@@ -0,0 +1,29 @@
+#
+# Copyright 2014 Alien Laboratories, Inc.
+#
+
+from injector import inject
+from config import Config
+from py2neo import Node, Relationship
+import random
+
+# http://py2neo.org/2.0/cypher.html#cypher-resource
+
+@inject(config=Config)
+class Database(object):
+
+    def __str__(self):
+        items = self.config.graph.cypher.execute('MATCH (i:Item) RETURN i')
+        return 'Graph({:d})'.format(len(items))
+
+    def add(self):
+        items = self.config.graph.cypher.execute('MATCH (i:Item) RETURN i')
+        name = 'Item-{:d}'.format(len(items) + 1)
+        item1 = Node('Item', name=name)
+
+        if items:
+            item2 = random.sample(items.to_subgraph().nodes, 1)[0]
+            rel = Relationship(item2, 'linked', item1)
+            self.config.graph.create(rel)
+        else:
+            self.config.graph.create(item1)
--- src/main/python/view.py
+++ src/main/python/view.py
@@ -3,9 +3,9 @@
 #
 
 import flask.views
-from py2neo import Node, Relationship
 from injector import inject
 from config import Config
+from data import Database
 
 
 @inject(config=Config)
@@ -17,20 +17,12 @@ class HomeView(flask.views.MethodView):
         return flask.render_template('home.html', config=self.config)
 
 
-@inject(config=Config)
+@inject(db=Database)
 class DataView(flask.views.MethodView):
 
     PATH = '/data'
 
     def get(self):
-        p1 = Node("Person", name="Alice")
-        p2 = Node("Person", name="Bob")
-        rel = Relationship(p1, "friend", p2)
-        self.config.graph.create(rel)
-
-        # TODO(burdon): find root node and auto-init.
-        for record in self.config.graph.cypher.execute("MATCH (p:Person) RETURN p.name AS name"):
-            print(record[0])
-
-        info = str(rel)
+        self.db.add()
+        info = str(self.db)
         return flask.render_template('data.html', info=info)
