e474d42a0525112d7650e127017d3fede82d70c6
NAME <EMAIL>
2015-05-08 21:39:36 -0400

    Tox and setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='DarkZero',
+    version='1.0',
+    description='Dark Zero',
+    author='Alien Labs',
+    author_email='info@alienlabs.io',
+    url='https://github.com/richburdon/dark-zero',
+    packages=[]
+)
